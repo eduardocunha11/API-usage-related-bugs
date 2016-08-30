31013e16b81b5f8a4a98f947eaaf237540d69ba8
eduardocunha11 <eduardocunha11@gmail.com>
2016-08-30 14:11:47 -0300

    Create misuse.java

---
+++ Java/5/misuse.java
@@ -0,0 +1,10 @@
+package my.test;
+
+public class ClassA<T> {
+	public static void main(String[] args) {
+		for(int i=1;i<100;i=i*2+1) {
+			System.out.println(Integer.toBinaryString(916));
+		}
+	}
+}
+
