66082b356fcbd2c800564f5254b480db1e48271c
eduardocunha11 <eduardocunha11@gmail.com>
2016-08-30 14:41:21 -0300

    Create misuse.js

---
+++ JavaScript/25/misuse.js
@@ -0,0 +1,17 @@
+var Bert = require('./bert').Bert,
+    BertTuple = require('./bert').BertTuple,
+    BertBinary = require('./bert').BertBinary
+    
+var puts = require('sys').puts
+var term = [
+    false, 2, 
+    new BertBinary([132,177,33,86].map(function(a){return String.fromCharCode(a)}).join(')),
+    new BertTuple(
+        [[
+            new BertTuple([new BertBinary('name'), 
+            new BertBinary('Emma')]), 
+            new BertTuple([new BertBinary('age'), 3])
+        ]]
+    ),
+    []]
+puts(Bert.pp_bytes(Bert.encode(term)));
